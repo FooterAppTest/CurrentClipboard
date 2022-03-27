ce24c47049531df616288237fd530ad95cde667c
FooterAppTest <98423844+FooterAppTest@users.noreply.github.com>
2022-03-28 00:07:29 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,9 +11,8 @@ window.writeValues = function(form) {
     document.getElementById('page').innerHTML =  '<a href="https://www.unum.pl" style="color:#004e8f">www.unum.pl</a><br>';
     document.getElementById('company-data').innerHTML =  "Unum Życie TUiR S.A., al. Jana Pawła II 17, 00-854 Warszawa, Spółka wpisana do rejestru przedsiębiorców w Sądzie Rejonowym dla m.st. Warszawy, XIII Wydział Gospodarczy Krajowego Rejestru Sądowego, KRS 0000008906, NIP 526-22-62-838, REGON 013281575, kapitał zakładowy 60 000 000,00 zł w całości opłacony.<br>";
     document.getElementById('rodo-data').innerHTML =  "Niniejsza wiadomość elektroniczna przeznaczona jest wyłącznie dla osób lub podmiotów, do których jest zaadresowana. Wgląd w treść wiadomości otrzymanej omyłkowo, dalsze jej rozpowszechnianie, innego rodzaju wykorzystanie lub podjęcie jakichkolwiek działań w oparciu o zawarte w niej informacje przez osoby lub podmioty niebędące jej adresatem, jest niedozwolone.<br>";
-    document.getElementById('eco').innerHTML =  "Chrońmy wspólnie środowisko i nie drukujmy tej wiadomości, jeśli nie jest to konieczne.<br><br><br>";
+    document.getElementById('eco').innerHTML =  "Chrońmy wspólnie środowisko i nie drukujmy tej wiadomości, jeśli nie jest to konieczne.";
   }
- 
 
   if(photo.value = "online"){
     document.forms[0].elements[6].options[0].value='<a href="http://www.fundacjaunum.pl/">' + '<img src="https://www.unum.pl/wp-content/uploads/2022/03/online.jpg" style="width: 500px;">' + '</a>';
@@ -22,7 +21,6 @@ window.writeValues = function(form) {
   if(photo.value = "fundacja"){
     document.forms[0].elements[6].options[1].value='<a href="http://www.fundacjaunum.pl/">' + '<img src="https://www.unum.pl/wp-content/uploads/2022/03/Fundacja_Unum_Banners_2500x600_s1.png" style="width: 500px;">' + '</a>';
   } 
-
 
   function CopyToClipboard(containerid) {
   var range = document.createRange();
@@ -33,3 +31,13 @@ window.writeValues = function(form) {
   window.getSelection().removeAllRanges();
   alert("Podpis został skopiowany");
 }
+
+var generateBtn = document.getElementById("generatebutton");
+var copyBtn = document.getElementById("copyButton");
+
+generateBtn.addEventListener("click", function(){
+  generateBtn.classList.add("clicked");
+  if (generateBtn.classList == "btn clicked"){
+    copyBtn.classList.add("show")
+  }
+})
